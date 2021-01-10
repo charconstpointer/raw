21484df6f6e3a318f55e097e329da46eea58da0c
charconstpointer <attwistingnether@gmail.com>
2021-01-10 06:14:43 +0100

    protov

--- pkg/raw/header.go
+++ pkg/raw/header.go
@@ -6,17 +6,24 @@ import (
 
 type Header []byte
 
-const HeaderSize = 8
+const HeaderSize = 10
+
+const ProtoVersion = 1
 
 func (h Header) Encode(len uint32, id uint32) {
-	binary.BigEndian.PutUint32(h[:4], len)
-	binary.BigEndian.PutUint32(h[4:8], id)
+	binary.BigEndian.PutUint16(h[:2], ProtoVersion)
+	binary.BigEndian.PutUint32(h[2:6], len)
+	binary.BigEndian.PutUint32(h[6:], id)
+}
+
+func (h Header) ProtoVersion() uint16 {
+	return binary.BigEndian.Uint16(h[:2])
 }
 
 func (h Header) Next() uint32 {
-	return binary.BigEndian.Uint32(h[:4])
+	return binary.BigEndian.Uint32(h[2:6])
 }
 
 func (h Header) ID() uint32 {
-	return binary.BigEndian.Uint32(h[4:8])
+	return binary.BigEndian.Uint32(h[6:])
 }
